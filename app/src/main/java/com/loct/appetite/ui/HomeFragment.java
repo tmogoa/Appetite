96ed8fd1a12a4515648491cce734839e47e4c9b6
Tony Mogoa <mogoa.tonny@gmail.com>
2021-08-11 17:53:55 +0300

    Basic App Structure done.

---
+++ app/src/main/java/com/loct/appetite/ui/HomeFragment.java
@@ -0,0 +1,40 @@
+package com.loct.appetite.ui;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.loct.appetite.R;
+
+public class HomeFragment extends Fragment {
+
+//    private HomeViewModel homeViewModel;
+//    private FragmentHomeBinding binding;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+//        homeViewModel =
+//                new ViewModelProvider(this).get(HomeViewModel.class);
+//
+//        binding = FragmentHomeBinding.inflate(inflater, container, false);
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+        return view;
+
+//        final TextView textView = binding.textHome;
+//        homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+//            @Override
+//            public void onChanged(@Nullable String s) {
+//                textView.setText(s);
+//            }
+//        });
+//        return root;
+    }
+}
