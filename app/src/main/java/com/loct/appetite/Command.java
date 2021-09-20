0dd097426e5b45e6e0468828776a391b1909b505
levizwannah <levizwannah@gmail.com>
2021-09-20 05:08:26 +0300

    Added the command interface and the firebase storage implementation to the gradle file. Also updated the gradle file.

---
+++ app/src/main/java/com/loct/appetite/Command.java
@@ -0,0 +1,5 @@
+package com.loct.appetite;
+
+public interface Command {
+    void execute();
+}
--- app/src/main/java/com/loct/appetite/ui/NewDishFragment.java
+++ app/src/main/java/com/loct/appetite/ui/NewDishFragment.java
@@ -7,10 +7,17 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 import com.loct.appetite.R;
 
 public class NewDishFragment extends Fragment {
+
+    private EditText dishName,
+            disDescription, price, priceMini;
+            
+    private ImageView dishImageView;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
